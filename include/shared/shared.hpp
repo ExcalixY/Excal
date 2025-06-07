ae402cce46743337f14461d38df050cfdbdca26f
ExcalixY <renaspenas1221@gmail.com>
2025-06-07 23:24:46 +0300

    Test commit

---
+++ include/shared/shared.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <cstdint>
+#define MAX_STACK_SIZE 4098
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
 #include "../include/shared/shared.hpp"
+#include <cstddef>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+  std::ifstream input("test");
+
+  return 0;
+}
